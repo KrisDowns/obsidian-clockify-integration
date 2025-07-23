3dcb5045cbd9bdd9a512a656870c57cab439959b
Kris Downs <KrisDowns@users.noreply.github.com>
2025-07-23 17:33:19 -0400

    Update PluginSettings.ts to work with 2 workspaces

--- lib/config/PluginSettings.ts
+++ lib/config/PluginSettings.ts
@@ -1,41 +1,14 @@
-import type { ClockifyWorkspace } from "../model/ClockifyWorkspace";
-
 export interface PluginSettings {
-  /**
-   * The user's Clockify API key.
-   */
-  apiToken: string;
+	// Single API Key for your Clockify account
+	clockifyApiKey: string;
 
-  /**
-   * The Clockify workspace to be used for the user's timer.
-   */
-  workspace: ClockifyWorkspace;
+	// ID for your "Work" workspace
+	workWorkspace: string;
 
-  /** Has dismissed the update alert for 0.4.0 */
-  hasDismissedAlert?: boolean;
-  
-  /** Update the day's total time in real-time in the sidebar */
-  updateInRealTime?: boolean;
+	// ID for your "Learning" workspace
+	learningWorkspace: string;
 
-  /**
-   * The max. allowed characters in the title of a timer in
-   * the status bar.
-   */
-  charLimitStatusBar: number;
-
-  /**
-   * The time format for the status bar.
-   */
-  statusBarFormat?: string;
-
-  /**
-   * The prefix to show before the time entry in the status bar.
-   */
-  statusBarPrefix?: string;
-
-  /** Whether to show the project in the status bar. */
-  statusBarShowProject?: boolean;
-
-  /** Message shown in the status bar when no time entry is running. */
-  statusBarNoEntryMesssage?: string;
+	// General Settings
+	showNewFeatureNotification: boolean;
+	lastUsedVersion: string;
 }
