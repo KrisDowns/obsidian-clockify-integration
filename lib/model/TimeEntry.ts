ecc4f0041e4184ff4e85f644637b207bcd9d5b1f
Kris Downs <KrisDowns@users.noreply.github.com>
2025-07-23 17:19:42 -0400

    Update TimeEntry.ts

--- lib/model/TimeEntry.ts
+++ lib/model/TimeEntry.ts
@@ -1,57 +1,23 @@
-/**
- * @deprecated
- */
-export interface TimeEntryStart {
-  /**
-   * The description for the new time entry
-   */
-  description: string;
-  /**
-   * The id of the project to start the new time entry on
-   */
-  project_id: number;
-  /**
-   * a list of tag names (optional)
-   */
-  tag_ids?: string[];
-}
+import type { Project } from './Project';
+import type { Tag } from './Tag';
 
-/**
- * Time entry data from the Toggl Track API.
- */
+// This model is adapted for Clockify's API response.
+export interface TimeEntry {
+	id: string;
+	description: string;
+	projectId: string;
+	tagIds: string[];
+	userId: string;
+	workspaceId: string;
+	timeInterval: {
+		start: string;
+		end: string | null;
+		duration: number | null;
+	};
 
-/**
- * @deprecated
- */
-export interface TimeEntry extends TimeEntryStart {
-  /**
-   * time entry id
-   */
-  id: number;
-  /**
-   * duration in milliseconds
-   */
-  duration: number;
-  /**
-   * start time in ISO 8601 date and time format
-   * (YYYY-MM-DDTHH:MM:SS)
-   */
-  start: string;
-  /**
-   * end time in ISO 8601 date and time format
-   * (YYYY-MM-DDTHH:MM:SS)
-   */
-  end: string;
-  /**
-   * project name
-   */
-  project?: string;
-  /**
-   * project color in hex format
-   */
-  project_hex_color?: string;
-  /**
-   * Workspace id
-   */
-  workspace_id: number;
+	// These are for internal use after processing
+	start?: string;
+	duration?: number;
+	project?: Project;
+	tags?: Tag[];
 }
