694a109aaf6987bf82849767433871aa17084abf
Kris Downs <KrisDowns@users.noreply.github.com>
2025-07-23 18:31:55 -0400

    Update vite.config.ts run in library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    // ...
-  },
+	plugins: [
+		svelte({
+			preprocess: [sveltePreprocess({ postcss: true })],
+		}),
+	],
+	build: {
+		// Set up for building a library
+		lib: {
+			// The entry point for the library is main.ts
+			entry: path.resolve(__dirname, 'main.ts'),
+			// The name of the global variable when used in a browser
+			name: 'obsidian-clockify-reports',
+			// The output filename for the bundle
+			fileName: () => 'main.js',
+			// We want to output a single 'es' module
+			formats: ['es'],
+		},
+		rollupOptions: {
+			// Make sure to externalize dependencies that shouldn't be bundled
+			// into your library. Obsidian and moment are provided by Obsidian itself.
+			external: ['obsidian', 'moment'],
+		},
+		// Don't clear the output directory before building
+		emptyOutDir: false,
+		// Output the files directly to the root directory
+		outDir: '.',
+	},
 });
