293aa0c5f8153394ed32de3aec6c02d21a2c4c6b
Kris Downs <KrisDowns@users.noreply.github.com>
2025-07-23 16:25:56 -0400

    Create ClockifyWorkspace.ts

---
+++ lib/model/ClockifyWorkspace.ts
@@ -0,0 +1,11 @@
+export interface ClockifyWorkspace {
+  /**
+   * The Clockify workspace ID (string format)
+   */
+  id: string;
+
+  /**
+   * The name of the workspace.
+   */
+  name: string;
+}
