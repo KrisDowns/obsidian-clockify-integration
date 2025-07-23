fc3bd844e2f807e08b388e8e70e2bd9fd6806d3d
Kris Downs <KrisDowns@users.noreply.github.com>
2025-07-23 17:19:02 -0400

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,14 +1,6 @@
-/**
- * The interval in ms at which the active timer is polled
- */
-export const ACTIVE_TIMER_POLLING_INTERVAL = 6000;
+export const TOGGL_API_URL = 'https://api.track.toggl.com/api/v9';
+export const TOGGL_REPORTS_API_URL = 'https://api.track.toggl.com/reports/api/v3';
+export const CLOCKIFY_API_URL = 'https://api.clockify.me/api/v1';
 
-/**
- * The interval in ms at which the status bar is updated
- */
-export const STATUS_BAR_UPDATE_INTERVAL = 1000;
-
-/**
- * The language string used for report code blocks.
- */
-export const CODEBLOCK_LANG = "toggl";
+export const VIEW_TYPE_TOGGL_REPORTS = 'toggl-reports';
+export const VIEW_TYPE_TOGGL_SIDEBAR_PANE = 'toggl-sidebar';
